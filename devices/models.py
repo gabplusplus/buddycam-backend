36711877c4d17d6afab5507d238766c536968824
Gabriel Leocario <gdleocario@ccc.edu.ph>
2023-02-01 15:04:53 +0800

    UUID update

--- devices/models.py
+++ devices/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-import uuid
+from shortuuid.django_fields import ShortUUIDField
 from members.models import Members
 
 class Devices(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = ShortUUIDField(primary_key=True, length=8, editable=False)
     device_ip = models.CharField(max_length=255)
     device_full_name = models.ForeignKey(Members, on_delete=models.CASCADE, to_field='full_name', related_name='deviceFullName')
     device_batt = models.CharField(max_length=3, default='0')
